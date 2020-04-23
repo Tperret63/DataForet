2fc3255221ad02b9724eb06e4949838a791b91db
MarieLaure-Martin <ml.martin@netc.fr>
2020-04-23 19:21:51 +0200

    PU variable

---
+++ R/PUvar.R
@@ -0,0 +1,16 @@
+#' Grille de prix variables
+#'
+#' @description Mercuriale des prix du bois ventilée par année, essence, grosseur et qualité.
+#'
+#' @format data frame
+#' 
+#' @param Année
+#' @param Essence
+#' @param Classe = catégorie de diamètre 
+#' @param PU = prix unitaire de l'année (euros/francs courants)
+#' @param Qual = qualité (3m pour les feuillus et 6m pour les résineux)
+#' 
+#' @examples
+#' data(PUvar)
+#' 
+#' @source Prix basés sur ceux publiés annuellement par la revue Forêt Privée.
