7bcc3caa09c2e20a6748d0917afa51adb48fdb9a
MarieLaure-Martin <ml.martin@netc.fr>
2020-04-24 10:08:50 +0200

    Coef de transformation euros-franc (INSEEpa)

---
+++ R/INSEEpa.R
@@ -0,0 +1,15 @@
+#' Coefficients de transformation de l'euro 
+#'
+#' @description  Coefficient de transformation de l'euro ou du franc d'une année, en euro ou franc d'une autre année - Base 2015
+#'
+#' @format data frame
+#' 
+#' @param Année
+#' @param Infla =  Coefficient de transformation
+#' @param Monnaie = euros (à partir de 2001) ou franc
+#' 
+#' @examples
+#' data(INSEEpa)
+#' 
+#' @source \url{https://www.insee.fr/fr/statistiques/serie/010605954#Tableau}
+"INSEEpa"
