bcb87749ccfb0f779859bca605480ac45df2b6be
MarieLaure-Martin <ml.martin@netc.fr>
2020-04-27 17:43:43 +0200

    Ajout données infradensité et coef vol tot Emerge

---
+++ R/VolEmerge.R
@@ -0,0 +1,15 @@
+#' Coefficients par essence pour le tarif de cubage issu du projet EMERGE (volume total)
+#'
+#' @description Coefficients du tarif de cubage issu du projet EMERGE pour calculer le volume total d'un arbre, par essence 
+#'
+#' @format data frame
+#' 
+#' @param Essence
+#' @param a = sans unité, spécifique à l'essence
+#' @param b = robustesse, commune pour tous les feuillus ou tous les résineux
+#' @param c = défilement, sans unité, spécifique à l'essence 
+#' 
+#' @examples
+#' data(VolEmerge)
+#' 
+#' @source C. Deleuze, F. Morneau, J.P. Renaud, Y. Vivien, M. Rivoire, et al.. Estimer le volume total d’un arbre, quelles que soient l’essence, la taille, la sylviculture, la station. Rendez-vous Techniques ONF, 2014, pp.22-32. hal-01143797
