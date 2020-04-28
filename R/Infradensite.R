3f2ecbc818db24176f448e0a0f4af381afc49bfd
MaxBruciamacchie <max.bruciamacchie@agroparistech.fr>
2020-04-28 15:30:56 +0200

    mise a jour

---
+++ R/Infradensite.R
@@ -0,0 +1,13 @@
+#' Infradensités pour les principales essences françaises
+#'
+#' @description Infradensités (tMS/m3) recommandées pour les principales essences forestières françaises utilisées par l'IGN.
+#'
+#' @format data frame
+#' 
+#' @param Essence
+#' @param infradensite = en tMS/m3
+#' 
+#' @examples
+#' data(Infradensite)
+#' 
+#' @source IGN, d'après Dupouey, 2002.
---
+++ R/PUafi.R
@@ -0,0 +1,15 @@
+#' Grille de prix fixes de l'Association Futaie Irrégulière (AFI)
+#'
+#' @description Mercuriale fixe des prix du bois ventilée par essence, grosseur et qualité. Ces prix ont été fixés par consensus entre les experts membres de l’association, en considérant des prix moyens du marché de l’année 1995. 
+#'
+#' @format data frame
+#' 
+#' @param Essence
+#' @param Classe = catégorie de diamètre 
+#' @param PU = prix unitaire fixe
+#' @param Qual = qualité (3m pour les feuillus et 6m pour les résineux)
+#' 
+#' @examples
+#' data(PUafi)
+#' 
+#' @source Association Futaie Irrégulière
