9734f26af65725504804d31e8d34888b330e673a
MarieLaure-Martin <ml.martin@netc.fr>
2020-04-28 13:57:49 +0200

    Fonction tarif cubage Emerge

--- R/VolEmerge.R
+++ R/VolEmerge.R
@@ -1,15 +1,17 @@
 #' Coefficients par essence pour le tarif de cubage issu du projet EMERGE (volume total)
 #'
-#' @description Coefficients du tarif de cubage issu du projet EMERGE pour calculer le volume total d'un arbre, par essence 
+#' @description Coefficients du tarif de cubage issu du projet EMERGE pour calculer le volume total d'un arbre (volume total aérien, jusqu'à extrêmité des rameaux), par essence
 #'
 #' @format data frame
 #' 
-#' @param Essence
-#' @param a = sans unité, spécifique à l'essence
-#' @param b = robustesse, commune pour tous les feuillus ou tous les résineux
-#' @param c = défilement, sans unité, spécifique à l'essence 
+#' @param CodeIFN = code de l'essence dans la base de données de l'IFN
+#' @param Essence = essence pour lesquelles le modèle a été testé et validé statistiquement
+#' @param A = sans unité, spécifique à l'essence
+#' @param B = robustesse, commune pour tous les feuillus ou tous les résineux
+#' @param C = défilement, sans unité, spécifique à l'essence 
 #' 
 #' @examples
 #' data(VolEmerge)
+#' Fonction associée Tarif_EMERGE() pour le calcul
 #' 
 #' @source C. Deleuze, F. Morneau, J.P. Renaud, Y. Vivien, M. Rivoire, et al.. Estimer le volume total d’un arbre, quelles que soient l’essence, la taille, la sylviculture, la station. Rendez-vous Techniques ONF, 2014, pp.22-32. hal-01143797
---
+++ Scripts/Tarif_EMERGE.R
@@ -0,0 +1,35 @@
+#' Fonction qui calcule le volume total d'un arbre selon le tarif de cubage du projet EMERGE
+#'
+#' @description Prend en entrée un data frame avec des mesures par arbre de hauteur totale (m) et circonférence (cm) et selon l'essence identifiée par le code IFN
+#'
+#' @format Renvoie le data frame avec une nouvelle colonne VTot
+#' 
+#' @param df = data frame avec les mesures par arbre
+#' @param Hauteur = variable de la hauteur totale en m
+#' @param Circ130 = circonférence mesurée à 1,30m en cm
+#' 
+#' @examples
+#' df <- Tarif_EMERGE(df, df$Hauteur, df$Circ130)
+#' 
+#' @source 
+
+
+# Hauteur en m et Diam130 en cm : nécessite un df qui déclarer variable hauteur totale et circonférence
+Tarif_EMERGE <- function(df, Hauteur, Circ130){
+  load("data/VolEmerge.rda")
+  load("data/CodesEssIFN.rda")
+ 
+  df <- df %>% 
+    left_join(CodesEssIFN, by = c("espar"="code")) %>% 
+    left_join(VolEmerge[,1:2], by = c("espar"="CodeIFN")) %>%
+    mutate(Essence = ifelse(!is.na(Essence), Essence,
+                            ifelse(grepl("76",espar) | grepl("74",espar) | grepl("73",espar)
+                                   | grepl("72",espar)| grepl("70",espar)
+                                   | grepl("77",espar)| grepl("69",espar)
+                                   | grepl("68",espar)| grepl("67",espar)
+                                   | grepl("66",espar)| grepl("59",espar)
+                                   | grepl("55",espar),"Résineux", "Feuillus"))) %>% 
+    left_join(VolEmerge, by = c("Essence")) %>% 
+    mutate(VTot = ((Hauteur*(Circ130*10^-2)^2)/(4*pi*(1-(1.3/Hauteur))^2)) * (A + B*(sqrt(Circ130*10^-2)/Hauteur) + C*(Hauteur/(Circ130*10^-2))))
+  return(df)
+}
