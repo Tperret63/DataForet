973915bed5ca759663148edc6ea7876b1ea29b6b
MaxBruciamacchie <max.bruciamacchie@agroparistech.fr>
2020-04-28 15:05:34 +0200

    Tarif Emerge

---
+++ Scripts/Tarif_EMERGE.R
@@ -0,0 +1,38 @@
+#' Volume total d'un arbre selon le tarif de cubage du projet EMERGE
+#'
+#' @description La fonction Tarif_EMERGE est un tarif à trois entrées propsé par l'ONF. 
+#' Elle nécessite en entrée un data frame contenant par arbre la hauteur totale (m), sa
+#' circonférence (cm) et l'essence identifiée par le code IFN.
+#'
+#' @format Renvoie le data frame avec une nouvelle colonne VTot
+#' 
+#' @param df = data frame avec les mesures par arbre
+#' @param Hauteur = variable de la hauteur totale en m
+#' @param Circ130 = circonférence mesurée à 1,30m en cm
+#' 
+#' @examples
+#' df <- Tarif_EMERGE(df, df$Hauteur, df$Circ130)
+#' @
+#' @source ONF
+
+
+# Hauteur en m et Diam130 en cm : nécessite un df qui déclarer variable hauteur totale et circonférence
+Tarif_EMERGE <- function(df, Hauteur, Circ130){
+  data(VolEmerge.rda)
+  data(CodesEssIFN.rda)
+ 
+  df <- df %>% 
+    left_join(CodesEssIFN, by = c("espar"="code")) %>% 
+    left_join(VolEmerge[,1:2], by = c("espar"="CodeIFN")) %>%
+    mutate(Essence = ifelse(!is.na(Essence), Essence,
+                            ifelse(grepl("76",espar) | grepl("74",espar) | grepl("73",espar)
+                                   | grepl("72",espar)| grepl("70",espar)
+                                   | grepl("77",espar)| grepl("69",espar)
+                                   | grepl("68",espar)| grepl("67",espar)
+                                   | grepl("66",espar)| grepl("59",espar)
+                                   | grepl("55",espar),"Résineux", "Feuillus"))) %>% 
+    left_join(VolEmerge, by = c("Essence")) %>% 
+    mutate(VTot = ((Hauteur*(Circ130*10^-2)^2)/(4*pi*(1-(1.3/Hauteur))^2)) * 
+             (A + B*(sqrt(Circ130*10^-2)/Hauteur) + C*(Hauteur/(Circ130*10^-2))))
+  return(df)
+}
--- Scripts/Utilisation.R
+++ Scripts/Utilisation.R
@@ -1,3 +1,38 @@
+# -------- INSEE --------
+## Mise à jour coefficient de transformation euros constants
+library(rvest)
+library(dplyr)
+
+MAJ_INSEEpa <- function(){
+  load("data/INSEEpa.rda")
+  
+  h <- read_html("https://www.insee.fr/fr/statistiques/serie/ajax/010605954") # lit la page web de l'INSEE avec les données 
+  PA <- html_node(h,"table") %>%  html_table() # récupère les coefficients sous forme de tableau directement de la page web
+  
+  new <- PA %>% 
+    mutate(Valeur = str_replace(Valeur, ",", ".")) %>% 
+    filter(Année == max(Année)) %>% 
+    mutate(Monnaie = 1) %>% 
+    rename(Infla = Valeur)
+  INSEEpa <- rbind(INSEEpa, new) %>% 
+    mutate(Infla =as.numeric(Infla))
+  
+  usethis::use_data(INSEEpa, overwrite = T)
+}
+
+
+# ------- Utilisation -----
+res <- IFNdata(FALSE)
+IFNarbres <- res$IFNArbres
+IFNplacettes <- res$IFNplacettes
+
+CodesIFNmod <- read_excel("/Users/maxbruciamacchie/Desktop/CodeIFN.xlsx", sheet=2)
+
+rnIFN <- st_read("~/pCloud Drive/Bureau/GeoData/Limites/Milieux/rn250_l93_shp-2/rnifn250_l93.shp") %>% 
+  select(REGN:geometry) %>% 
+  ms_simplify(keep=0.1, keep_shapes = T, drop_null_geometries = F)
+st_write(rnIFN, "rnIFN.shp")
+
 ser <- st_read("~/pCloud Drive/Bureau/GeoData/Limites/Milieux/ser_l93/ser_l93.shp") %>% 
   mutate(codeser = as.character(codeser),
          NomSER = as.character(NomSER)) %>% 
@@ -10,6 +45,13 @@ greco <- ser %>%
   summarise() %>% 
   filter(greco!="-")
 plot(st_geometry(greco))
+
+
+INSEEcom <- read.delim("~/pCloud Sync/Packages/DataForet/temp/INSEEcom.txt", encoding="latin1")
+INSEEdep <- read.delim("~/pCloud Sync/Packages/DataForet/temp/INSEEdep.txt", encoding="latin1") %>% 
+  select(-TNCC)
+INSEEreg <- read.delim("~/pCloud Sync/Packages/DataForet/temp/INSEEreg.txt", encoding="latin1") %>% 
+  select(-TNCC)
 
 # ------------- Sauvegarde
 usethis::use_data(IFNarbres, overwrite = T)
@@ -25,30 +67,4 @@ usethis::use_data(INSEEreg, overwrite = T)
 usethis::use_data(CodesIFNmod, overwrite = T)
 usethis::use_data(INSEEpa, overwrite = T)
 
-# ------------- Utilisation
-res <- IFNdata(FALSE)
-IFNarbres <- res$IFNArbres
-IFNplacettes <- res$IFNplacettes
 
-CodesIFNmod <- read_excel("/Users/maxbruciamacchie/Desktop/CodeIFN.xlsx", sheet=2)
-
-rnIFN <- st_read("~/pCloud Drive/Bureau/GeoData/Limites/Milieux/rn250_l93_shp-2/rnifn250_l93.shp") %>% 
-  select(REGN:geometry) %>% 
-  ms_simplify(keep=0.1, keep_shapes = T, drop_null_geometries = F)
-st_write(rnIFN, "rnIFN.shp")
-
-# ------------ INSEE --------
-INSEEcom <- read.delim("~/pCloud Sync/Packages/DataForet/temp/INSEEcom.txt", encoding="latin1")
-INSEEdep <- read.delim("~/pCloud Sync/Packages/DataForet/temp/INSEEdep.txt", encoding="latin1") %>% 
-  select(-TNCC)
-INSEEreg <- read.delim("~/pCloud Sync/Packages/DataForet/temp/INSEEreg.txt", encoding="latin1") %>% 
-  select(-TNCC)
-
-INSEEpa <- INSEEpa %>% 
-  rename(Monnaie = Coefft)
-new = data.frame(Année = 2019,
-                 Infla = 104.23,
-                 Monnaie = 1)
-INSEEpa <- rbind(INSEEpa, new)
-
-INSEEpa <- PA
