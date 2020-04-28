aa1e1fbfc9ffbcc53d80f712359651270fea6fc7
MarieLaure-Martin <ml.martin@netc.fr>
2020-04-28 16:55:33 +0200

    Données IFN arbres morts (foret)

--- R/IFNdata.R
+++ R/IFNdata.R
@@ -29,6 +29,7 @@ IFNdata <- function (enrg = TRUE) {
 
   IFNarbres <- data.table()
   IFNplacettes <- data.table()
+  IFNarbres_morts <- data.table()
 
   # --------- Boucle Import par annee
   page <- read_html("https://inventaire-forestier.ign.fr/spip.php?article707&ID_TELECHARGEMENT=18429&TOKEN=6e5cf5998c9b2f6842d050c1c61d43ec") # lit le lien IGN pour télécharger les données
@@ -61,6 +62,8 @@ IFNdata <- function (enrg = TRUE) {
       tabArbres$Annee <- an
       tabPlacettes <- read.csv2(liste[grepl(paste("placettes_foret_",an,sep=""), liste)])
       tabPlacettes$Annee <- an
+      tabMorts <- read.csv2(liste[grepl(paste("arbres_morts_foret_",an,sep=""), liste)])
+      tabMorts$Annee <- an
       OK = TRUE
     }
 
@@ -68,6 +71,7 @@ IFNdata <- function (enrg = TRUE) {
     if(OK) {
       IFNarbres <- rbindlist(list(IFNarbres, tabArbres), use.names=T, fill=T)
       IFNplacettes <- rbindlist(list(IFNplacettes, tabPlacettes), use.names=T, fill=T)
+      IFNarbres_morts <- rbindlist(list(IFNarbres_morts, tabMorts), use.names=T, fill=T)
       # --------- supression fichier et dossier temporaires
       unlink(temp); unlink(tempRep)
     }
@@ -78,18 +82,23 @@ IFNdata <- function (enrg = TRUE) {
   IFNarbres$v   <- as.numeric(as.character(IFNarbres$v))
   IFNarbres$w   <- as.numeric(as.character(IFNarbres$w))
   IFNarbres$htot <- as.numeric(as.character(IFNarbres$htot))
+  IFNarbres_morts$v   <- as.numeric(as.character(IFNarbres_morts$v))
+  IFNarbres_morts$w   <- as.numeric(as.character(IFNarbres_morts$w))
   # --------- Selection colonnes
   IFNarbres <- IFNarbres %>%
     dplyr::select(idp,a,espar,veget,mortb,acci,ori,mortb,c13,ir5,htot,hdec,v,w,Annee)
   IFNplacettes <- IFNplacettes %>%
     dplyr::select(idp,xl93,yl93,ser,csa,dc,dist,Annee)
+  IFNarbres_morts <- IFNarbres_morts %>%
+    dplyr::select(idp,a,veget,datemort,espar,ori,c13,v,w,Annee)
   # --------- Creation archive ou return
   if(enrg) {
     usethis::use_data(IFNarbres, overwrite = T)
     usethis::use_data(IFNplacettes, overwrite = T)
+    usethis::use_data(IFNarbres_morts, overwrite = T)
   } else {
-    out = list(IFNarbres, IFNplacettes)
-    names(out) <- c("IFNarbres", "IFNplacettes")
+    out = list(IFNarbres, IFNplacettes, IFNarbres_morts)
+    names(out) <- c("IFNarbres", "IFNplacettes","IFNarbres_morts")
     return(out)
   }
 }
