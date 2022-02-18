767ef9caf7d8497e1ea08223f568b538a7054237
Bruciamacchie <max.bruciamacchie@gmail.com>
2022-02-18 11:51:02 +0100

    Communes

---
+++ R/Communes.R
@@ -0,0 +1,10 @@
+#' Communes de la France métropolitaine.
+#'
+#' @description fichier géoréférencé des communes de la France métropolitaine
+#'
+#' @format Data frame contenant 34736 lignes et 1 variable.
+#' 
+#' @param INSEE = code Insee
+#' 
+#' @examples
+#' data(Communes)
